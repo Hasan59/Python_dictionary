f87fc6281246a00bd3f850476622948d3a23a014
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 00:42:14 +0600

    The Genda Ful

--- python_dictionary.py
+++ python_dictionary.py
@@ -181,3 +181,65 @@ if "model" in thisdict:
         for x in thisdict.keys():
           print(x)
 
+          thisdict = {
+            "brand": "Ford",
+            "model": "Mustang",
+            "year": 1964
+          }
+          for x, y in thisdict.items():
+            print(x, y)
+
+            thisdict = {
+              "brand": "Ford",
+              "model": "Mustang",
+              "year": 1964
+            }
+            mydict = thisdict.copy()
+            print(mydict)
+
+            thisdict = {
+              "brand": "Ford",
+              "model": "Mustang",
+              "year": 1964
+            }
+            mydict = dict(thisdict)
+            print(mydict)
+
+            myfamily = {
+              "child1": {
+                "name": "Emil",
+                "year": 2004
+              },
+              "child2": {
+                "name": "Tobias",
+                "year": 2007
+              },
+              "child3": {
+                "name": "Linus",
+                "year": 2011
+              }
+            }
+
+            print(myfamily)
+
+            child1 = {
+              "name": "Emil",
+              "year": 2004
+            }
+            child2 = {
+              "name": "Tobias",
+              "year": 2007
+            }
+            child3 = {
+              "name": "Linus",
+              "year": 2011
+            }
+
+            myfamily = {
+              "child1": child1,
+              "child2": child2,
+              "child3": child3
+            }
+
+            print(myfamily)
+
