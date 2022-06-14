3fb84f8fc8c16c4244c265a41971c0431a0ecd86
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 00:25:18 +0600

    The Genda Ful

--- python_dictionary.py
+++ python_dictionary.py
@@ -116,3 +116,68 @@ if "model" in thisdict:
   }
   thisdict.update({"color": "red"})
   print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict.pop("model")
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict.popitem()
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  del thisdict["model"]
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict.clear()
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  for x in thisdict:
+    print(x)
+
+    thisdict = {
+      "brand": "Ford",
+      "model": "Mustang",
+      "year": 1964
+    }
+    for x in thisdict:
+      print(thisdict[x])
+
+      thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+      }
+      for x in thisdict.values():
+        print(x)
+
+        thisdict = {
+          "brand": "Ford",
+          "model": "Mustang",
+          "year": 1964
+        }
+        for x in thisdict.keys():
+          print(x)
+
