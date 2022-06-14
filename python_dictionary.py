23661977fb10dedca383dddb7fd9bf2e7b20db01
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 00:19:45 +0600

    The Gash Ful

--- python_dictionary.py
+++ python_dictionary.py
@@ -62,3 +62,57 @@ print(x) #before the change
 car["year"] = 2020
 
 print(x) #after the change
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict["year"] = 2018
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict.update({"year": 2020})
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict["color"] = "red"
+  print(thisdict)
+
+  thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+  }
+  thisdict.update({"color": "red"})
+  print(thisdict)
